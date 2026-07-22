58b7a5b79cb92446cd0894435d3a698ffc56e75a
Sanghyuk Park <sanghyuk.park85@gmail.com>
2026-07-22 13:39:05 -0500

    add game constants

---
+++ src/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <iostream>
+
+namespace constants
+{
+inline constexpr size_t screen_width{100};
+inline constexpr size_t screen_height{50};
+} // namespace constants
+
+#endif
